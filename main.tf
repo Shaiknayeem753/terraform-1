5c401a94d8f3a7256ecdef7185a536b37ecf137a
tesst123 <test>
2022-08-27 20:22:25 +0530

    Sg attachent

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_instance" "foo" {
 
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
   security_group_id    = aws_security_group.sg.id
-  network_interface_id = data.aws_instance.instance.network_interface_id
+  network_interface_id = aws_network_interface.foo.id
 }
 
 
