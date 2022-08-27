e74f2d1b8f6fbafbb2588942e034161efb752c9b
tesst123 <test>
2022-08-27 19:56:49 +0530

    az remove

--- main.tf
+++ main.tf
@@ -72,3 +72,30 @@ resource "aws_instance" "ec2" {
       }
   
 }
+
+
+resource "aws_security_group" "sg" {
+  name        = "all traffic"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "all traffic"
+  }
+}
